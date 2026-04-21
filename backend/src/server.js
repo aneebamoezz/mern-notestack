996e8284123b5fbdcf2c7a4d4e58c5018af1aeaa
Aneeba Moezz <anibakaleem@gmail.com>
2026-04-22 03:31:29 +0500

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,42 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv";
+
+import noteRoutes from "./routes/noteRoutes.js"
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+
+
+dotenv.config()
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+//middleware
+app.use(cors(
+    {
+        origin:"http://localhost:5173",
+    }
+))
+app.use(express.json()) //this middleware will parse JSONbodies: req.body
+app.use(rateLimiter);
+
+//our simple custom middleware
+// app.use((req, res, next) => {
+//     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//     next();
+// })
+
+app.use("/api/notes/", noteRoutes)
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT:", PORT);
+    })
+});
+
+
